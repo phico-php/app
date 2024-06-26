764c8b8e32dbb200a047777d0bae806e41c3423d
indgy <me@indgy.uk>
2024-06-26 12:22:17 +0100

    Added example for global error handler

--- app/events.php
+++ app/events.php
@@ -6,3 +6,17 @@
 
 // $app->on($code, function($request, $e) {});
 
+// handle all non-http errors
+$app->on('error', function ($request, $e) {
+
+    if ($request->isXhr()) {
+        return response($e->getCode())->json([
+            'status' => 'error',
+            'message' => 'Sorry an error occured, we will look into it'
+        ]);
+    }
+
+    // return html
+    return response($e->getCode())->html('<h1>Server Error</h1>');
+
+});
